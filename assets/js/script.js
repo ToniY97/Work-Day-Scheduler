428f3c3d8ee36007b447f33a5d348c2c84fe5ef7
Toni97 <ayovcheva75@gmail.com>
2024-01-09 15:28:55 +0000

    code for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,37 @@ $(document).ready(function () {
     $("#currenDay").text(daysjs().format("dddd, MMMM D"));
 
     //Create time blocks
+    for (let i= 9; i <= 17; i++) {
+        let timeBlock = $("<div>").addClass("row time-block");
+        let hour = $("<div>").addClass("hour col-md-1").text(dayjs().hour(i).format("hA"));
+        let textArea = $("<textarea>").addClass("col=md-10");
+        let saveBtn = $("<button>").addClass("saveBtn col-md-1").html("<i class='fas fa-save'></i>");
 
-    //Set background colour based on past, present future
+        //Set background colour based on past, present future
+        if (i < dayjs().hour()) {
+            textArea.addClass("past");
+        } else if (i === dayjs().hour()) {
+            textArea.addClass("present");
+        }else {
+            textArea.addClass("future");
+        }
+
+        //Retrieve saved event from local storage
+        let savedEvent = localStorage.getItem("event_" + i);
+        if (savedEvent) {
+            textArea.val(savedEvent);
+        }
+
+           //Save button click event
+           saveBtn.on("click", function () {
+            let eventText = textArea.val();
+            localStorage.setItem("event_" + i, eventText);
+           });
+    }
+    
 
 
-    //Retrieve saved event from local storage
+    
 
-    //Save button click event
+ 
 })
