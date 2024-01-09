3632b8d1155059d45b3befb7fffb339c215d1841
Toni97 <ayovcheva75@gmail.com>
2024-01-09 14:59:28 +0000

    added algorithm comments

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    //Display current day
+    $("#currenDay").text(daysjs().format("dddd, MMMM D"));
+
+    //Create time blocks
+
+    //Set background colour based on past, present future
+
+
+    //Retrieve saved event from local storage
+
+    //Save button click event
+})
